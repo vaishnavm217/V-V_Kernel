39e645209f43ff0b4ffdf69161fd3773526cd70b
Vaishnav Murali <vaishnavmurali@gmail.com>
2019-09-23 07:03:49 +0200

    build_script: Modified Build script
    Signed-off-by: Vaishnav Murali <vaishnavmurali@gmail.com>

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,5 @@
 # !/usr/bin/sh
-make O=out ARCH=arm64 CROSS_COMPILE=~/aarch64gcc/bin/aarch64-linux-android- merge_hi6250_defconfig > defconfig.log
+make O=out ARCH=arm64 CROSS_COMPILE=~/aarch64gcc/bin/aarch64-linux-android- merge_hi6250_defconfig >&defconfig.log
 
 if [ $? -eq 0 ]; then
 	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
@@ -11,18 +11,10 @@ fi
 
 telegram-send -f defconfig.log
 
-make O=out ARCH=arm64 CROSS_COMPILE=~/aarch64gcc/bin/aarch64-linux-android- -j200 > defconfig.log > build.log
+make O=out ARCH=arm64 CROSS_COMPILE=~/aarch64gcc/bin/aarch64-linux-android- -j200 >&build.log
 if [ $? -eq 0 ]; then
 	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
 	telegram-send $TIME_CURR" - Build successful"
-else
-	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
-	telegram-send $TIME_CURR" - Error in building"
-fi
-
-telegram-send -f build.log
-
-if [ $? -eq 0 ]; then
 	./tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x00400000 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x10000000 --os_version 9 --os_patch_level 2019-04-01  --output kernelvv.img
 	id=$(gdrive upload kernelvv.img --share)
 	TOKEN=`echo $id | grep -Po 'Uploaded \K.*?(?= at)'|tr -d '\n\r'`
@@ -30,10 +22,15 @@ if [ $? -eq 0 ]; then
 	telegram-send 'Build Image Successful
 	Token:'$TOKEN'
 	Link: '$Link
-	rm -rf kernelvv.img defconfig.log build.log
+	rm -rf kernelvv.img
+else
+	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
+	telegram-send $TIME_CURR" - Error in building"
 fi
 
-rm -rf out/
+telegram-send -f build.log
+
+rm -rf out/ defconfig.log build.log
 make mrproper
 make clean
 
